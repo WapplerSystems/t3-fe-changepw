01de40112b601cb6a7c16d2f56c99acedd21de18
Sven Wappler <wappler@wappler.systems>
2025-02-06 16:10:06 +0100

    init

---
+++ ext_emconf.php
@@ -0,0 +1,23 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'fe change password',
+    'description' => 'Change password form for frontend users based on form',
+    'category' => 'plugin',
+    'author' => 'Sven Wappler',
+    'author_email' => '',
+    'author_company' => 'WapplerSystems',
+    'state' => 'stable',
+    'clearCacheOnLoad' => 0,
+    'version' => '12.0.0',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '12.4.0-12.4.99',
+            'form' => '12.4.0-12.4.99',
+        ],
+        'conflicts' => [
+        ],
+        'suggests' => [
+        ],
+    ],
+];
